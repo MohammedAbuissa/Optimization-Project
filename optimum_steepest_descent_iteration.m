581fd166c67592dcccc24c235c8dab2b1d43f981
Mohammed Abuissa <mohammed.abuissa@outlook.com>
2016-12-21 09:02:40 +0200

    Fix optimum_steepest_descent_iteration

--- optimum_steepest_descent_iteration.m
+++ optimum_steepest_descent_iteration.m
@@ -1,17 +1,33 @@
-function [new_point, optimality] = optimum_steepest_descent_iteration(symbols, init_point, obj_function, min)
-grad = transpose(gradient(obj_function, symbols));
-if(min)
-    gradient_factor = -1;
-else
-    gradient_factor = 1;
-end
-syms lambda
-lambda_point(lambda) = init_point + gradient_factor * lambda * subs(grad, symbols, init_point);
-lambda_value = transpose(solve(subs(obj_function, symbols, lambda_point), lambda));
-new_point = lambda_point(lambda_value(1));
-if(norm(subs(grad, symbols, new_point)) < 0.0001)
-    optimality = true;
-else
-    optimality = false;
-end
+function [new_point, function_value, optimality] = optimum_steepest_descent_iteration(symbols, init_point, obj_function, min, tolerance)
+    grad = transpose(gradient(obj_function, symbols));
+    if(min)
+        gradient_factor = -1;
+    else
+        gradient_factor = 1;
+    end
+    
+    syms lambda
+    
+    lambda_point(lambda) = init_point + gradient_factor * lambda * subs(grad, symbols, init_point);
+    lambda_value = transpose(vpasolve(gradient(subs(obj_function, symbols, lambda_point), lambda), lambda));
+    
+    %invalid lambda values
+    if(isempty(lambda_value))
+        error('cant find optimal step size');
+    elseif (isa(lambda_value, 'sym'))
+        lambda_value = eval(lambda_value);
+        if(isnan(lambda_value))
+            error('step size is not defined');
+        end
+    end
+            
+    new_point = eval(lambda_point(real(lambda_value(1))));
+    
+    function_value = eval(subs(obj_function, symbols, new_point));
+    
+    if(norm(subs(grad, symbols, new_point)) < tolerance)
+        optimality = true;   
+    else
+        optimality = false;
+    end
 end
--- testScript.m
+++ testScript.m
@@ -1,4 +1,4 @@
-n = [100,10];
+n = [3.2,3.3];
 o = 0;
 syms x
 syms y
@@ -6,13 +6,14 @@ de_jong_2d = x.^2 + y.^2;
 rosenbrock_valley = 100 * (y-x.^2)^2 + (1-x).^2;
 rastigin = 10 *2 + (x.^2-10*cos(2*pi*x)) + (y.^2 - 10 * cos(2*pi*y));
 easom = -cos(x) * cos(y) * exp(-(x-pi).^2 - (y-pi).^2);
-
+branin = (y-(5.1/(4*pi^2))*x^.2+(5/pi)*x-6).^2 + 10*(1- (1/(8*pi)))*cos(x)+10;
 iterations_count = 0;
 
 while(o == false)
-[n, o] = optimum_steepest_descent_iteration([x y], n, rosenbrock_valley, true);
+[n, min ,o] = optimum_steepest_descent_iteration([x y], n, easom, true,0.001);
 iterations_count = iterations_count +1;
 end
 
 disp(n);
 disp(iterations_count);
+disp(min);
